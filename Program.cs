533b583ca373f63ce8cc03a32156a8dc90a1d2f8
Anthony Salemo <ajssalemo@gmail.com>
2021-09-09 18:28:07 -0400

    added a quickstart dotnet -o console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dapr_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
