0041c93be14fa2ee0ad13338487597a6fb8ef679
Anthony Salemo <ajssalemo@gmail.com>
2021-09-12 18:26:07 -0400

    mapped the frontend root path to a controller

---
+++ DaprBackEnd/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DaprBackEnd.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public String Index()
+        {
+            var message = "DaprBackend";
+            return message;
+        }
+    }
+}
