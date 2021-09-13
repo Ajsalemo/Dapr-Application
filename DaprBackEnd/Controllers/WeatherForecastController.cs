a6c95f742a31cad4f4b517e17f0c79521bfbbea4
Anthony Salemo <ajssalemo@gmail.com>
2021-09-13 13:40:11 -0400

    able to run this locally and validate the services are calling between eachother. need to implement the dapr dashboard

--- DaprBackEnd/Controllers/WeatherForecastController.cs
+++ DaprBackEnd/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace DaprBackEnd.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Breezy"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
